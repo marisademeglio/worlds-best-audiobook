0a228ab2655c23db3f8e8c0425e76cc052d90aed
Marisa DeMeglio <marisa.demeglio@gmail.com>
2019-12-21 23:35:19 -0800

    Added script to clean up gutenberg EPUB2 output
    Outputs HTML5 plus a readingOrder

--- utils/README.md
+++ utils/README.md
@@ -2,12 +2,18 @@
 
 ## convert
 
-`npm run convert -- --input ./example/audacity-labels.txt --meta ./example/meta.json`
+`npm run convert -- --input ./example/audacity-labels.txt --config ./example/audacity-labels-meta.json`
 
-Produces a Synchronized Narration file with the name of the audio file specified in `meta.json`.
+Produces a Synchronized Narration file with the name of the audio file specified in `audacity-labels-meta.json`.
 
 Does not include HTML yet.
 
+## split
+
+`npm run gutensplit -- --input ./example/blue.epub --config ./example/blue-config.json`
+
+Splits a Gutenberg EPUB2 into one-per-chapter HTML5 files. Produces JSON with a reading order. Options provided in `blue-config.json`.
+
 ### TODO: 
 
-* include HTML. As this is a special-purpose script, we can be strict about what we expect from the HTML in order to match up the sync points automatically. E.g. use only headings and paragraphs in sequence. Or, follow elements with IDs according to a certain scheme.
+* Script to merge audiobook with gutensplit output, plus audacity labels or other timing info, and get an audiobook with sync narr points. 
---
+++ utils/splittenberg.js
@@ -0,0 +1,201 @@
+// process an EPUB 2 file
+// unzips it, reads the TOC, makes one HTML5 file per TOC entry, named accordingly
+// export a manifest.json readingOrder
+// works best when the nav points are a flat list and the HTML is basically a flat list too, contained within <body>
+// e.g. this should work great on project gutenberg EPUBs
+
+const fs = require('fs-extra');
+const path = require('path');
+const program = require('commander');
+const extractZip = require('extract-zip');
+const tmp = require('tmp');
+const xpath = require('xpath');
+const DOMParser = require('xmldom-alpha').DOMParser;
+const XMLSerializer = require('xmldom-alpha').XMLSerializer;
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+program.version('0.0.1');
+program
+    .requiredOption('-i, --input <file>', 'EPUB file')
+    .option('-c, --config <file>', 'Config JSON file (see example)')
+    .option('-f, --force', 'Overwrite existing output');
+program.parse(process.argv);
+
+let config = program.config ? JSON.parse(fs.readFileSync(path.resolve(__dirname, program.config)).toString()) : null;
+
+if (!fs.existsSync(path.resolve(__dirname, 'out'))){
+    fs.mkdirSync(path.resolve(__dirname, 'out'));
+}
+
+main();
+
+async function main() {
+    let tmpdir = await unzip(path.resolve(__dirname, program.input));
+
+    const select = xpath.useNamespaces({
+        html: 'http://www.w3.org/1999/xhtml',
+        epub: 'http://www.idpf.org/2007/ops',
+        ncx: 'http://www.daisy.org/z3986/2005/ncx/',
+        opf: 'http://www.idpf.org/2007/opf',
+        dc: 'http://purl.org/dc/elements/1.1/'
+      });
+    
+      
+      const packageDocPath = calculatePackageDocPath(tmpdir);
+
+      // merge all the content docs into one giant one
+      const packagedoc = new DOMParser({errorHandler}).parseFromString(fs.readFileSync(packageDocPath).toString());
+      const spineItemIdrefs = select('//opf:itemref/@idref', packagedoc);
+      let allBodyChildren = [];
+      spineItemIdrefs.map(idref => {
+        const manifestItem = select(`//opf:item[@id='${idref.nodeValue}']`, packagedoc);
+        const filepath = path.join(path.dirname(packageDocPath), manifestItem[0].getAttribute('href'));
+        const contentDoc = new DOMParser({errorHandler}).parseFromString(fs.readFileSync(filepath).toString());
+        const bodyElm = select('//html:body', contentDoc);
+        Array.from(bodyElm[0].childNodes).map(node => {
+            if ((node.hasOwnProperty('tagName') && node.tagName == 'br') 
+                || 
+                (node.textContent.trim() != '' && node.textContent.trim() != '\n')
+            ) {
+                allBodyChildren.push(node);
+            }
+        })
+      });
+
+  
+
+    const dom = new JSDOM(`<!DOCTYPE html><html><head></head><body>${allBodyChildren.join('')}</body></html>`);
+    const bigContentDoc = dom.window.document;
+    let allNodes = Array.from(bigContentDoc.querySelectorAll("*"));
+    allNodes.map(node => {
+        node.removeAttribute("class");
+        node.removeAttribute("xmlns");
+        node.removeAttribute('tag');
+        node.removeAttribute('xml:space');
+    });
+    
+    const tocPath = path.resolve(path.dirname(packageDocPath), select('//opf:item[@id="ncx"]', packagedoc)[0].getAttribute('href'));
+
+    const tocdoc = new DOMParser({errorHandler}).parseFromString(
+        fs.readFileSync(tocPath).toString(), 
+        'application/x-dtbncx+xml');
+    let navPointElms = select("//ncx:navPoint", tocdoc);
+
+    console.log(navPointElms
+    .filter(elm => config.skipNcxIds.includes(elm.getAttribute("id") == false)));
+
+    let readingOrder = navPointElms
+    .filter(elm => config.skipNcxIds.includes(elm.getAttribute("id")) == false)
+    .map(navPoint => {
+        let startFrag = select("ncx:content/@src", navPoint)[0].value;
+        startFrag = startFrag.split("").reverse().join("");
+        startFrag = startFrag.substr(0, startFrag.indexOf("#"));
+        startFrag = startFrag.split("").reverse().join("");
+        let name = select("ncx:navLabel/ncx:text", navPoint)[0].textContent; 
+        return {
+            name: name,
+            url: `${name.replace(/ /gi, '-')}.html`,
+            start: startFrag
+        };
+    });
+
+    console.log(readingOrder);
+
+    readingOrder.map((item, idx) => {
+      let nextItem = idx + 1 < readingOrder.length ? readingOrder[idx+1] : "END";
+      let nodes = [];
+      let startElm = bigContentDoc.querySelector("#" + item.start);
+      let nextElm = startElm;
+      let parentElm = startElm.parentElement;
+      if (startElm.nodeName == "H2") {
+        let modHeading = bigContentDoc.createElement('h1');
+        modHeading.innerHTML = nextElm.innerHTML;
+        let subsequentElm = startElm.nextSibling;
+        parentElm.removeChild(startElm);
+        if (subsequentElm) {
+          parentElm.insertBefore(modHeading, subsequentElm);
+        }
+        else {
+          parentElm.appendChild(modHeading);
+        }
+        nextElm = modHeading;
+      }
+
+      
+      while (nextElm != null && nextElm.getAttribute("id") != nextItem.start) {
+        nodes.push(nextElm);
+        nextElm = nextElm.nextSibling;
+      }
+
+      const itemDom = new JSDOM(
+        `<!DOCTYPE html><html><title>${item.name}</title><head></head><body>${nodes.map(n=>n.outerHTML).join('')}</body></html>`);
+      
+      let outfile = `${item.url}`;
+      let outpath = path.resolve(__dirname, 'out/', outfile);
+
+      writeOut(outpath, itemDom.serialize());
+
+    });
+
+    let manifest = {
+      readingOrder: readingOrder.map(item => ({name: item.name, url: item.url}))
+    };
+    
+    writeOut(`${path.basename(program.input).replace('.epub', '')}.json`, JSON.stringify(manifest));
+    
+
+}
+
+
+
+function writeOut(filename, contents) {
+  let outpath = path.resolve(__dirname, 'out/', filename);
+  if (fs.existsSync(outpath) && program.force || !fs.existsSync(outpath)) {
+    fs.writeFileSync(outpath, contents);
+    console.log(`Wrote ${outpath}`);
+  } 
+  else {
+      console.log(`ERROR: Cannot overwrite existing file of the same name:\n ${outpath}\nUse --force.\n`);
+  }
+}
+
+
+
+// UNZIP
+
+tmp.setGracefulCleanup();
+
+async function unzip(path) {
+  const tmpdir = tmp.dirSync({ unsafeCleanup: true }).name;
+  return new Promise((resolve, reject) => {
+    extractZip(path, { dir: tmpdir }, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(tmpdir);
+      }
+    });
+  })
+}
+
+const errorHandler = {
+    warning: w => console.log("WARNING: ", w),
+    error: e => console.log("ERROR: ", e),
+    fatalError: fe => console.log("FATAL ERROR: ", fe),
+}
+
+function calculatePackageDocPath (epubDir) {
+    const containerFilePath = `${epubDir}/META-INF/container.xml`;
+    const content = fs.readFileSync(containerFilePath).toString();
+    const doc = new DOMParser({errorHandler}).parseFromString(content);
+    const select = xpath.useNamespaces({ ocf: 'urn:oasis:names:tc:opendocument:xmlns:container' });
+    const rootfiles = select('//ocf:rootfile[@media-type="application/oebps-package+xml"]/@full-path', doc);
+    // just grab the first one as we're not handling the case of multiple renditions
+    if (rootfiles.length > 0) {
+      return (path.join(epubDir, rootfiles[0].nodeValue));
+    }
+    return '';
+}
+  
+  
