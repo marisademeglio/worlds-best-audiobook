2cc4e833f312451be0cdc8237f282d5574d6c7b0
Marisa DeMeglio <marisa.demeglio@gmail.com>
2020-04-30 21:36:40 -0700

    Use user-selected font size if available; click an element to play it
    Also fixed the flash on loading

--- web/player/iframe.js
+++ web/player/iframe.js
@@ -1,7 +1,11 @@
+import * as Events from './events.js';
+
 async function initIframe(url, parentSelector) {
     return new Promise((resolve, reject) => {
         let content = document.querySelector(parentSelector);
         content.innerHTML = '';
+        // disable the iframe parent element while we change the content and apply a stylesheet
+        content.classList.add('disabled');
         let iframe = document.createElement('iframe');
         iframe.onload = () => {
             log.debug(`iframe loaded ${url}`);
@@ -13,6 +17,21 @@ async function initIframe(url, parentSelector) {
                     iframeStyle.setAttribute('href', new URL('css/pub-default.css', document.location.href));
                     let iframeHead = iframe.contentDocument.querySelector('head');
                     iframeHead.appendChild(iframeStyle);
+                    if (localStorage.getItem("fontsize")) {
+                        iframe.contentDocument.querySelector("body").style.fontSize = localStorage.getItem("fontsize");
+                    }
+
+                    // a bit hacky but ensures we are only listening for clicks in the main text area
+                    // and not the TOC
+                    if (parentSelector.indexOf("player-page") != -1) {
+                        let allSyncedElms = Array.from(iframe.contentDocument.querySelectorAll("*[id]"));
+                        allSyncedElms.map(elm => {
+                            elm.addEventListener("click", e => {
+                                Events.trigger("Document.Click", e.target.getAttribute("id"));
+                            });
+                        });
+                    }
+                    
                     resolve(iframe.contentDocument);
                 }
                 else {
@@ -24,6 +43,8 @@ async function initIframe(url, parentSelector) {
                 log.warn("Can't access iframe content doc");
                 resolve(null);
             }
+            // a short delay prevents the screen from flashing as it becomes un-disabled
+            setTimeout(() => content.classList.remove('disabled'), 300);
         };
         iframe.setAttribute('src', url);
         content.appendChild(iframe);
