16d005fd5d52cc0ec5d3d070b3f213aabca30c08
Marisa DeMeglio <marisa.demeglio@gmail.com>
2020-05-04 12:21:51 -0700

    WIP: mobile debugging

--- web/library/index.js
+++ web/library/index.js
@@ -9,41 +9,42 @@ document.addEventListener("DOMContentLoaded", () => {
 async function populateTitles() {
     console.log(titles);
     // titles are defined in the main library document
-    Promise.all(
-        titles.map(async title => {
-            // open each manifest and get the title + cover image
-            let manifest = new Manifest();
-            manifest.setSupportedProfiles([{
-                id: 'https://www.w3.org/TR/audiobooks/',
-                encodingFormats: ['audio/mpeg']
-            }]);
-            let url = new URL(title, document.location.href);
-            await manifest.loadUrl(url);
-            let cover = manifest.getCover();
-            if (!cover || !isImage(cover.encodingFormat)) {
-                cover = null;
-            }
-            return {
-                url, 
-                title: manifest.getTitle(),
-                cover
-            };
-        })
-    )
-    .then(titlesData => {
-        titlesData.map(titleData => {
-            let titleListElm = document.querySelector("#titles");
-            let titleListItem = document.createElement("li");
-            titleListItem.innerHTML = `
-                <a href="../player/?q=${titleData.url}">
-                    <img src="${titleData.cover.url}" alt="Cover for ${titleData.title}">
-                </a>
-                <a href="../player/?q=${titleData.url}">
-                ${titleData.title}
-                </a>
-            `;
-            titleListElm.appendChild(titleListItem);
-        });  
-    });
+    let i;
+    let titlesData = [];
+    for (i=0; i<titles.length; i++) {
+        let title = titles[i];
+        let manifest = new Manifest();
+        manifest.setSupportedProfiles([{
+            id: 'https://www.w3.org/TR/audiobooks/',
+            encodingFormats: ['audio/mpeg']
+        }]);
+        let url = new URL(title, document.location.href);
+        await manifest.loadUrl(url);
+        let cover = manifest.getCover();
+        if (!cover || !isImage(cover.encodingFormat)) {
+            cover = null;
+        }
+        titlesData.push({
+            url, 
+            title: manifest.getTitle(),
+            cover
+        });
+    }
+
+    console.log(titlesData);
+    
+    titlesData.map(titleData => {
+        let titleListElm = document.querySelector("#titles");
+        let titleListItem = document.createElement("li");
+        titleListItem.innerHTML = `
+            <a href="../player/?q=${titleData.url}">
+                <img src="${titleData.cover.url}" alt="Cover for ${titleData.title}">
+            </a>
+            <a href="../player/?q=${titleData.url}">
+            ${titleData.title}
+            </a>
+        `;
+        titleListElm.appendChild(titleListItem);
+    });  
 }
 
